c8041271c45d43e377ab210dd7712ba761154878
Mark Chouinard <mark@chouinard.me>
2019-07-14 15:38:12 -0700

    Add percentage to doughnut hole.

--- src/components/ninety-chart.js
+++ src/components/ninety-chart.js
@@ -7,17 +7,58 @@ const meetingChart = () => {
 
 	console.log( 'from ninety-chart.js' );
 
+	const data = {
+		labels: [ 'done', 'remaining' ],
+		datasets: [ {
+			label: 'Meeting Count',
+			data: [ meetingCount, remaining ],
+			backgroundColor: [ 'green', 'yellow' ],
+		} ],
+	};
+
+	const legendDisplay = 'pie' === geojson.chartType ? true : false;
+
+	const options = {
+		responsive: true,
+		legend: {
+			display: legendDisplay
+		}
+	};
+
 	new Chart( ctx, {
 		type: geojson.chartType,
-		data: {
-			labels: [ 'done', 'remaining' ],
-			datasets: [ {
-				label: 'Meeting Count',
-				data: [ meetingCount, remaining ],
-				backgroundColor: [ 'green', 'red' ],
-			} ],
-		},
+		data: data,
+		options: options
 	} );
 };
+
+/**
+ * Add percentage to center of Doughnut chart.
+ *
+ * @see https://stackoverflow.com/a/34947440/774793
+ */
+if ( 'doughnut' === geojson.chartType ) {
+	Chart.pluginService.register(
+		{
+			beforeDraw: function ( chart ) {
+				var width = chart.chart.width,
+					height = chart.chart.height,
+					ctx = chart.chart.ctx;
+
+				ctx.restore();
+				var fontSize = ( height / 114 ).toFixed( 2 );
+				ctx.font = fontSize + "em sans-serif";
+				ctx.textBaseline = "middle";
+
+				var text = ( ( geojson.meetingCount / 90 ) * 100 ).toFixed( 1 ) + "%",
+					textX = Math.round( ( width - ctx.measureText( text ).width ) / 2 ),
+					textY = height / 2;
+
+				ctx.fillText( text, textX, textY );
+				ctx.save();
+			}
+		}
+	);
+}
 
 export default meetingChart;
