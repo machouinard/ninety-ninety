986b297d0d46dc9fe9cb2119dac032aeb169a4b4
Mark Chouinard <mark@chouinard.me>
2019-07-16 17:33:06 -0700

    Add ability to create PDF from date range.

--- src/admin/js/admin-index.js
+++ src/admin/js/admin-index.js
@@ -1,4 +1,15 @@
 jQuery( document ).ready( function ( $ ) {
+
 	$( '.ninety-color-field' ).wpColorPicker();
+	$( '.ninety-datepicker' ).datepicker();
+
+	// Handle click on "x" by datepicker fields to clear value.
+	$( '.pdf-clear' ).click(
+		e => {
+			// (e.target).previousSibling is the date input field.
+			( ( e.target ).previousSibling ).value = '';
+		}
+	);
+
 } );
 
