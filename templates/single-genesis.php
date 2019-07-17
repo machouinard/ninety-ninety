1fe1c101248e706d1d3192cce1dbdd3a9cacb2b7
Mark Chouinard <mark@chouinard.me>
2019-07-16 21:32:31 -0700

    Edit single Genesis template

--- templates/single-genesis.php
+++ templates/single-genesis.php
@@ -13,9 +13,9 @@ add_filter(
 		'ninety_meeting_genesis_entry_meta_header',
 	]
 );
-
-// Replace Entry content with our own.
-add_action( 'genesis_entry_content', 'ninety_meeting_entry_content' );
+//remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+// Extend Entry content with our own.
+add_action( 'genesis_entry_content', 'ninety_meeting_entry_content', 5 );
 /**
  * Output single Meeting content
  *
@@ -26,10 +26,11 @@ function ninety_meeting_entry_content() {
 
 //	$program  = get_field( 'ninety_meeting_program', get_the_ID() );
 //	$type     = get_field( 'ninety_meeting_type', get_the_ID() );
-	$location = get_field( 'ninety_meeting_location', get_the_ID() );
-	$notes    = get_field( 'ninety_meeting_notes', get_the_ID() );
-	$speaker  = get_field( 'ninety_meeting_speaker', get_the_ID() );
-	$topic    = get_field( 'ninety_meeting_topic', get_the_ID() );
+	$location      = get_field( 'ninety_meeting_location', get_the_ID() );
+	$notes         = get_field( 'ninety_meeting_notes', get_the_ID() );
+	$speaker       = get_field( 'ninety_meeting_speaker', get_the_ID() );
+	$topic         = get_field( 'ninety_meeting_topic', get_the_ID() );
+	$private_notes = get_field( 'ninety_meeting_private_notes', get_the_ID() );
 
 	if ( $location instanceof WP_Term ) {
 		echo '<h3><a href="/meetings/' . esc_attr( $location->slug ) . '">' . esc_html( $location->name ) . '</a></h3>';
@@ -49,11 +50,10 @@ function ninety_meeting_entry_content() {
 		echo '</p>';
 	}
 
-	if ( ! $notes ) {
-		$notes = __( 'No notes', 'ninety-ninety' );
+	// If we have notes and they're not private, put 'em on the page.
+	if ( $notes && ! $private_notes ) {
+		echo apply_filters( 'the_content', $notes );
 	}
-
-	echo apply_filters( 'the_content', $notes );
 
 	echo '<span class="locked-bottom"><a href="/meetings/">back to meetings</a></span>';
 
