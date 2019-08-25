e5c31b280890a33044835d3c1d5636373de6a37a
Mark Chouinard <mark@chouinard.me>
2019-08-25 14:01:42 -0700

    Modify files/directories to copy to dist

--- dist.sh
+++ dist.sh
@@ -2,13 +2,14 @@
 
 rm -rf ./dist
 mkdir ./dist
-cp -R ./assets dist
 cp -R ./inc dist
 cp -R ./lang dist
+cp -R ./assets dist
 cp -R ./templates dist
+cp -R ./child-themes dist
 cp ./index.php dist
-cp ./ninety-ninety.php dist
 cp ./Readme.md dist
+cp ./ninety-ninety.php dist
 
 mkdir ninety-ninety
 cp -R dist/* ninety-ninety
