52f92dc718a68ac0a4972cfe5b5b9805cb677c55
Mark Chouinard <mark@chouinard.me>
2019-08-25 21:33:36 -0700

    Update ACF to 5.8.3

--- inc/acf/includes/forms/form-post.php
+++ inc/acf/includes/forms/form-post.php
@@ -5,10 +5,10 @@ if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 if( ! class_exists('ACF_Form_Post') ) :
 
 class ACF_Form_Post {
-
+	
 	/** @var string The first field groups style CSS. */
 	var $style = '';
-
+	
 	/**
 	*  __construct
 	*
@@ -21,17 +21,17 @@ class ACF_Form_Post {
 	*  @return	void
 	*/
 	function __construct() {
-
+		
 		// initialize on post edit screens
 		add_action('load-post.php',		array($this, 'initialize'));
 		add_action('load-post-new.php',	array($this, 'initialize'));
-
+		
 		// save
 		add_filter('wp_insert_post_empty_content',		array($this, 'wp_insert_post_empty_content'), 10, 2);
 		add_action('save_post', 						array($this, 'save_post'), 10, 2);
 	}
-
-
+	
+	
 	/**
 	*  initialize
 	*
@@ -44,25 +44,25 @@ class ACF_Form_Post {
 	*  @return	void
 	*/
 	function initialize() {
-
+		
 		// globals
 		global $typenow;
-
+		
 		// restrict specific post types
 		$restricted = array('acf-field-group', 'attachment');
 		if( in_array($typenow, $restricted) ) {
 			return;
 		}
-
+		
 		// enqueue scripts
 		acf_enqueue_scripts(array(
 			'uploader'	=> true,
 		));
-
+		
 		// actions
 		add_action('add_meta_boxes', array($this, 'add_meta_boxes'), 10, 2);
 	}
-
+	
 	/**
 	*  add_meta_boxes
 	*
@@ -76,31 +76,31 @@ class ACF_Form_Post {
 	*  @return	void
 	*/
 	function add_meta_boxes( $post_type, $post ) {
-
+		
 		// Storage for localized postboxes.
 		$postboxes = array();
-
+		
 		// Get field groups for this screen.
 		$field_groups = acf_get_field_groups(array(
-			'post_id'	=> $post->ID,
+			'post_id'	=> $post->ID, 
 			'post_type'	=> $post_type
 		));
-
+		
 		// Loop over field groups.
 		if( $field_groups ) {
 			foreach( $field_groups as $field_group ) {
-
+					
 				// vars
 				$id = "acf-{$field_group['key']}";			// acf-group_123
 				$title = $field_group['title'];				// Group 1
 				$context = $field_group['position'];		// normal, side, acf_after_title
 				$priority = 'high';							// high, core, default, low
-
+				
 				// Reduce priority for sidebar metaboxes for best position.
 				if( $context == 'side' ) {
 					$priority = 'core';
 				}
-
+				
 				/**
 				 * Filters the metabox priority.
 				 *
@@ -111,7 +111,7 @@ class ACF_Form_Post {
 				 * @param	array $field_group The field group array.
 				 */
 				$priority = apply_filters('acf/input/meta_box_priority', $priority, $field_group);
-
+				
 				// Localize data
 				$postboxes[] = array(
 					'id'		=> $id,
@@ -120,31 +120,31 @@ class ACF_Form_Post {
 					'label'		=> $field_group['label_placement'],
 					'edit'		=> acf_get_field_group_edit_link( $field_group['ID'] )
 				);
-
+				
 				// Add the meta box.
 				add_meta_box( $id, $title, array($this, 'render_meta_box'), $post_type, $context, $priority, array('field_group' => $field_group) );
-
+				
 			}
-
+			
 			// Set style from first field group.
 			$this->style = acf_get_field_group_style( $field_groups[0] );
-
+			
 			// Localize postboxes.
 			acf_localize_data(array(
 				'postboxes'	=> $postboxes
 			));
 		}
-
+		
 		// remove postcustom metabox (removes expensive SQL query)
 		if( acf_get_setting('remove_wp_meta_box') ) {
-			remove_meta_box( 'postcustom', false, 'normal' );
+			remove_meta_box( 'postcustom', false, 'normal' ); 
 		}
-
+		
 		// Add hidden input fields.
 		add_action('edit_form_after_title', array($this, 'edit_form_after_title'));
-
+		
 		/**
-		*  Fires after metaboxes have been added.
+		*  Fires after metaboxes have been added. 
 		*
 		*  @date	13/12/18
 		*  @since	5.8.0
@@ -155,7 +155,7 @@ class ACF_Form_Post {
 		*/
 		do_action('acf/add_meta_boxes', $post_type, $post, $field_groups);
 	}
-
+	
 	/**
 	*  edit_form_after_title
 	*
@@ -168,23 +168,23 @@ class ACF_Form_Post {
 	*  @return	void
 	*/
 	function edit_form_after_title() {
-
+		
 		// globals
 		global $post, $wp_meta_boxes;
-
+		
 		// render post data
 		acf_form_data(array(
 			'screen'	=> 'post',
 			'post_id'	=> $post->ID
 		));
-
+		
 		// render 'acf_after_title' metaboxes
 		do_meta_boxes( get_current_screen(), 'acf_after_title', $post );
-
+		
 		// render dynamic field group style
 		echo '<style type="text/css" id="acf-style">' . $this->style . '</style>';
 	}
-
+	
 	/**
 	*  render_meta_box
 	*
@@ -198,16 +198,16 @@ class ACF_Form_Post {
 	*  @return	void
 	*/
 	function render_meta_box( $post, $metabox ) {
-
+		
 		// vars
 		$id = $metabox['id'];
 		$field_group = $metabox['args']['field_group'];
-
+		
 		// Render fields.
 		$fields = acf_get_fields( $field_group );
 		acf_render_fields( $fields, $post->ID, 'div', $field_group['instruction_placement'] );
 	}
-
+	
 	/**
 	*  wp_insert_post_empty_content
 	*
@@ -221,7 +221,7 @@ class ACF_Form_Post {
 	*  @return	bool
 	*/
 	function wp_insert_post_empty_content( $maybe_empty, $postarr ) {
-
+		
 		// return false and allow insert if '_acf_changed' exists
 		if( $maybe_empty && acf_maybe_get_POST('_acf_changed') ) {
 			return false;
@@ -230,7 +230,7 @@ class ACF_Form_Post {
 		// return
 		return $maybe_empty;
 	}
-
+	
 	/*
 	*  allow_save_post
 	*
@@ -245,35 +245,35 @@ class ACF_Form_Post {
 	*  @return	bool
 	*/
 	function allow_save_post( $post ) {
-
+		
 		// vars
 		$allow = true;
-
+		
 		// restrict post types
 		$restrict = array( 'auto-draft', 'revision', 'acf-field', 'acf-field-group' );
 		if( in_array($post->post_type, $restrict) ) {
 			$allow = false;
 		}
-
+		
 		// disallow if the $_POST ID value does not match the $post->ID
 		$form_post_id = (int) acf_maybe_get_POST('post_ID');
 		if( $form_post_id && $form_post_id !== $post->ID ) {
 			$allow = false;
 		}
-
+		
 		// revision (preview)
 		if( $post->post_type == 'revision' ) {
-
+			
 			// allow if doing preview and this $post is a child of the $_POST ID
 			if( acf_maybe_get_POST('wp-preview') == 'dopreview' && $form_post_id === $post->post_parent) {
 				$allow = true;
 			}
 		}
-
+		
 		// return
 		return $allow;
 	}
-
+	
 	/*
 	*  save_post
 	*
@@ -281,42 +281,42 @@ class ACF_Form_Post {
 	*
 	*  @type	function
 	*  @date	23/06/12
-	*  @since	0.1.0
+	*  @since	1.0.0
 	*
 	*  @param	int $post_id The post ID
 	*  @param	WP_POST $post the post object.
 	*  @return	int
 	*/
-
+	
 	function save_post( $post_id, $post ) {
-
+		
 		// bail ealry if no allowed to save this post type
 		if( !$this->allow_save_post($post) ) {
 			return $post_id;
 		}
-
+		
 		// verify nonce
 		if( !acf_verify_nonce('post') ) {
 			return $post_id;
 		}
-
+		
 		// validate for published post (allow draft to save without validation)
 		if( $post->post_status == 'publish' ) {
-
+			
 			// bail early if validation fails
 			if( !acf_validate_save_post() ) {
 				return;
 			}
 		}
-
+		
 		// save
 		acf_save_post( $post_id );
-
+		
 		// save revision
 		if( post_type_supports($post->post_type, 'revisions') ) {
 			acf_save_post_revision( $post_id );
 		}
-
+		
 		// return
 		return $post_id;
 	}
